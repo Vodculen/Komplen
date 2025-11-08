15ac8c44ac57a14f0b84fad3985af44f4849beb1
Vodculen <stefanaschen@gmail.com>
2025-11-08 13:52:56 -0500

    Added in groups to group related pages together!

--- src/components/layout/Chapters.jsx
+++ src/components/layout/Chapters.jsx
@@ -1,9 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 import Spacer from "@components/page/Spacer";
 import LinkedList from "@components/libraries/LinkedList";
-
 
 /**
  * @param {list} list The list of ChapterSections.
@@ -12,21 +11,25 @@ import LinkedList from "@components/libraries/LinkedList";
  * @returns All the ChapterSections in a list with some stylization.
  * @see ChapterSection()
  */
-export default function Chapters({ list, activeTabIndex }) {
+export default function Chapters({ list }) {
 	const location = useLocation();
 	const chaptersRef = useRef(null);
 	let tabCounter = 0;
 
 	const listItems = list.map((section, sectionIndex) => {
 		const tabsWithActive = section.tabs.map((tab) => {
-			const isActive = location.pathname.startsWith(tab.link);
-			const updatedTab = { ...tab, active: isActive ? "active" : "" };
+			if (tab.group) {
+				return { ...tab, isGroup: true };
+			} else {
+				const isActive = location.pathname.startsWith(tab.link);
 
-			tabCounter++;
-			return updatedTab;
+				tabCounter++;
+
+				return { ...tab, active: isActive ? "active" : "" };
+			}
 		});
 
-		return <ChapterSection key={sectionIndex} sectionName={section.title} list={tabsWithActive} />;
+		return <ChapterSection key={sectionIndex} sectionName={section.title} list={tabsWithActive} location={location} />;
 	});
 
 	return (
@@ -43,12 +46,52 @@ export default function Chapters({ list, activeTabIndex }) {
  *  
  * @returns The title and all the links under the title
  */
-function ChapterSection({ list, sectionName }) {
+function ChapterSection({ list, sectionName, location }) {
 	return (
 		<ul>
-			<h2 className="chaptersSection">{ sectionName }</h2>
-			<LinkedList list={list} />
+			<h2 className="chaptersSection">{sectionName}</h2>
+
+			{list.map((tab, i) =>
+				tab.isGroup ? (
+					<GroupedTabs key={i} group={tab.group} title={tab.name} location={location} />
+				) : (
+					<LinkedList key={i} list={[tab]} />
+				)
+			)}
+
 			<Spacer amount={32} />
 		</ul>
+	);
+}
+
+
+function GroupedTabs({ group, title, location }) {
+	const [isOpen, setIsOpen] = useState(false);
+	const toggleOpen = (e) => {
+		e.preventDefault();
+		setIsOpen((prev) => !prev);
+	};
+
+	const tabsWithActive = group.map((g) => ({
+		...g, active: location.pathname.startsWith(g.link) ? "active" : ""
+	}));
+
+	const Arrow = ({ open }) => (
+		<svg className="arrow" xmlns="http://www.w3.org/2000/svg" height="22px" viewBox="0 -960 960 960" width="22px" fill="#e3e3e3" style={{ transform: open ? "rotate(270deg)" : "rotate(0deg)", transition: "transform 0.2s", }}><path d="m432-480 156 156q11 11 11 28t-11 28q-11 11-28 11t-28-11L348-452q-6-6-8.5-13t-2.5-15q0-8 2.5-15t8.5-13l184-184q11-11 28-11t28 11q11 11 11 28t-11 28L432-480Z"/></svg>
+	);
+
+	return (
+		<div className="group">
+			<p onClick={toggleOpen} className="groupName">
+				<span>{title}</span>
+				<span><Arrow open={isOpen} /></span>
+			</p>
+
+			{isOpen && (
+				<div className="groupContent">
+					<LinkedList list={tabsWithActive} />
+				</div>
+			)}
+		</div>
 	);
 }
--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -3,19 +3,6 @@ import { defaultJsonLoader } from "@components/libraries/JsonLoaders";
 
 
 /**
- * Flattens the sidebar tab structure.
- */
-const flattenTabs = (list) => {
-	let tabs = [];
-
-	list?.forEach((section) => {
-		section.tabs.forEach((tab) => tabs.push(tab));
-	});
-
-	return tabs;
-};
-
-/**
  * @returns A fully filled out and stylized sidebar
  */
 export default function Sidebar() {
@@ -25,8 +12,5 @@ export default function Sidebar() {
 		return null;
 	}
 
-	const allTabs = flattenTabs(load);
-	const activeTabIndex = allTabs.findIndex((tab) => tab.link === location.pathname);
-
-	return <Chapters list={load} activeTabIndex={activeTabIndex} />;
+	return <Chapters list={load} />;
 }
